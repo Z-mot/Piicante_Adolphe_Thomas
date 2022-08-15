9112ba7787d08083f53831da5cc5ab3ddb3f11c2
Zmot <toto95270@hotmail.fr>
2022-08-15 13:08:04 +0200

    Code commenté

--- Backend/Models/user.js
+++ Backend/Models/user.js
@@ -1,11 +1,17 @@
+//importation de mongoose et de mongoose-unique-validator
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
+//création du schéma utilisateur avec la Class Schema du module mongoose
 const userSchema = mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
 
+/*utilisation de la méthode plugin de notre schéma qui prend en paramètre
+notre constante uniqueValidator afin de garantir chaque schéma unique*/
 userSchema.plugin(uniqueValidator);
 
+/*exportation de notre schéma via la fonction model de mongoose afin
+de rendre ce schéma disponible dans d'autres fichiers*/
 module.exports = mongoose.model("User", userSchema);
