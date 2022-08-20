7a1fea56688d3cd6452d3736944a4b49ff223793
Zmot <toto95270@hotmail.fr>
2022-08-20 17:38:11 +0200

    Création de la route like dans le fichier sauce.js du dossier Controllers

--- Backend/Controllers/sauce.js
+++ Backend/Controllers/sauce.js
@@ -2,6 +2,7 @@ const Sauce = require("../Models/sauce");
 const fs = require("fs");
 
 
+
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
@@ -20,13 +21,15 @@ exports.createSauce = (req, res, next) => {
     const sauce = new Sauce({
         ...sauceObject,
         userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/Images/${req.file.filename}`
-       
+        imageUrl: `${req.protocol}://${req.get('host')}/Images/${req.file.filename}`,  
+        likes: 0,
+        dislikes: 0
     });
+    console.log(sauce);
     sauce.save()
         .then(() => res.status(201).json({ message: "Sauce créée !" }))
         .catch(error => res.status(400).json({ error }));
-    };
+};
 
 exports.modifySauce = (req, res , next) => {
     const sauceObject = req.file ? {
@@ -35,7 +38,7 @@ exports.modifySauce = (req, res , next) => {
     } : { ...req.body };
     delete sauceObject.userId;
     Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
+        .then(sauce => {
             if(sauce.userId != req.auth.userId) {
                 res.status(403).json({ message : "Non Autorisé" })
             } else {
@@ -62,4 +65,56 @@ exports.deleleteSauce = (req, res, next) => {
             }
         })
         .catch(error => res.status(500).json({ error }));
+};
+
+exports.like = (req, res, next) => {
+    const voted = req.body.like;
+    const user = req.body.userId;
+    let tabLikes = [];
+    let tabDislikes= [];
+
+    //récupérer la sauce dans la BDD
+    //récupérer le tableau d'usersLiked et usersDisliked
+    //vérifier l'action de l'utilisateur : si l'utilisateur à déjà émis un vote il sera dans l'un des deux array
+    //mettre à jour la BDD
+    //tabLikes.push( user );
+    //tabDislikes.push(user);
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            tabLikes = sauce.usersLiked;
+            tabDislikes = sauce.usersDisliked;
+            if (tabLikes.indexOf(user) != -1) {
+                //l'user a déjà liké
+                if (voted == 0) {
+                    const index = tabLikes.indexOf(user);
+                    if (index > -1) { 
+                        tabLikes.splice(index, 1); 
+                    }
+                }
+            } else {
+                if (tabDislikes.indexOf(user) != -1) {
+                    //l'user à déjà disliké
+                    if (voted == 0) {
+                        const index = tabDislikes.indexOf(user);
+                        if (index > -1) {
+                            tabDislikes.splice(index, 1);
+                        }
+                    }
+                } else {
+                    // l'user n'a pas donné son avis
+                    if (voted == 1) {//like
+                        tabLikes.push(user);
+                    } else {//dislike
+                        tabDislikes.push(user);
+                    }
+                }
+            }
+            console.log(tabLikes.length, tabDislikes.length);
+            //mettre à jour la sauce dans la BDD
+            Sauce.updateOne({ _id: req.params.id }, { likes: tabLikes.length, dislikes: tabDislikes.length , usersLiked: tabLikes, usersDisliked: tabDislikes, _id: req.params.id })
+                .then(() => res.status(200).json({ message: "Sauce évaluée par l'user"}))
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+
 };
