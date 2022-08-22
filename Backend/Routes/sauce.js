b70e054b5bc9f505fb43ad1aa2ef9cf07f9ba9c3
Zmot <toto95270@hotmail.fr>
2022-08-22 11:15:40 +0200

    Fichier sauce.je du dossier Routes commenté

--- Backend/Routes/sauce.js
+++ Backend/Routes/sauce.js
@@ -1,3 +1,6 @@
+/*importation de : express, des Middlewares auth(pour l'authentification par token)
+ et multer-config(pour la prise en charge des fichiers images), du controller sauce qui contient 
+ toutes les logiques métiers*/
 const express = require("express");
 const auth = require("../Middlewares/auth");
 const multer = require("../Middlewares/multer-config");
@@ -5,6 +8,10 @@ const sauceCtrl = require("../Controllers/sauce");
 
 const router = express.Router();
 
+/*implémentation de toutes les routes sauces de l'API avec en premier paramètre
+le end-point, suivi du middleware d'authentification pour renforcer la sécurité
+puis du middleware multer pour les routes utilisant la gestion de fichier image
+et enfin de tous les middleware comprenant les logiques métiers propre à chaque route*/
 router.get("/", auth, sauceCtrl.getAllSauces);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.post("/", auth, multer, sauceCtrl.createSauce);
@@ -12,4 +19,5 @@ router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 router.delete("/:id", auth, sauceCtrl.deleleteSauce);
 router.post("/:id/like", auth, sauceCtrl.like);
 
+//exportation des routes afin qu'elles soient utilisées dans d'autres fichiers
 module.exports = router;
